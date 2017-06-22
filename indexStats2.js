65173c4e02a45cbb9caa2f314565f9bda750136a
James Turner <james.turner.phpninja@gmail.com>
2017-06-22 11:17:21 +0100

    A faster implementation of index stats

---
+++ indexStats2.js
@@ -0,0 +1,41 @@
+function indexStats() {
+var indexes = {}
+var collections = db.getCollectionNames()
+for(var cIdx in collections) {
+    var cName = collections[cIdx];
+    var nsName = db.getName()+"."+cName;
+    if(cName.indexOf("system") == -1) {
+        db.system.profile.find({ns:nsName}).batchSize(1000).forEach(function(profileDoc){
+            try {
+                if(profileDoc.query["$query"]) {
+                    var explain = db[cName].find(profileDoc.query["$query"]).explain();
+                    var index = explain.cursor.split(" ")[1]
+                } else {
+                    var explain = db[cName].find(profileDoc.query).explain();
+                    var index = explain.cursor.split(" ")[1]
+                }
+                indexes[index] = (indexes[index]||0)+1
+            } catch(e) {
+                // ignore exceptions...
+            }
+        })
+
+    }
+}
+
+for(var cIdx in collections) {
+    var cName = collections[cIdx];
+    if(cName.indexOf("system") == -1) {
+        print("checking for unused indexes in: " + cName);
+        for(var iIdx in db[cName].getIndexes()) {
+            var iName = db[cName].getIndexes()[iIdx].name;
+            if(iName.indexOf("system") == -1) {
+                if(!indexes[iName]) {
+                    print("this index is not being used: ");
+                    printjson(iName);
+                }
+            }
+        }
+    }
+}
+}
